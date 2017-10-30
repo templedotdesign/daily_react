fc486b287b6622bd9af19d279c0640ecc7e2f25f
Ryan Temple <temple.design.me@gmail.com>
2017-10-30 17:18:59 -0400

    preparing to eject

--- test_app/src/App.js
+++ test_app/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Test from './Test/Test';
+
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Test App</h1>
+        <p>App Component</p>
+        <Test/>
       </div>
     );
   }
---
+++ test_app/src/Test/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Test.css';
+
+const test = () => {
+  return(
+    <div className="test">
+      <p>Test</p>
+    </div>
+  );
+}
+
+export default test;
